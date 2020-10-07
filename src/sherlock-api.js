1b1b27b85cfbfa3087fe760460bb40b3097db514
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-10-06 21:34:27 -0700

    adds api function routes

---
+++ src/sherlock-api.js
@@ -0,0 +1,47 @@
+import request from 'superagent';
+
+const URL = process.env.APP_API_URL;
+
+export function signUp(userData) {
+  request.post(`${URL}/api/v1/auth/signup`, userData);
+
+  return request.get(`${URL}/api/v1/auth/verify`);
+}
+
+export function login(userData) {
+  request.post(`${URL}/api/v1/auth/login`, userData);
+
+  return request.get(`${URL}/api/v1/auth/verify`);
+}
+
+export function getPortfolio() {
+  try {
+    return request.get(`${URL}/api/v1/auth/verify`);
+  } catch(e) {
+    return { error: e.message }
+  }
+}
+
+export function insertStock(stock) {
+  try {
+    return request.post(`${URL}/api/v1/stocks`, stock);
+  } catch(e) {
+    return { error: e.message }
+  }
+}
+
+export function updateStock(stockId, stockUpdate) {
+  try {
+    return request.put(`${URL}/api/v1/stocks/${stockId}`, stockUpdate);
+  } catch(e) {
+    return { error: e.message }
+  }
+}
+
+export function deleteStock(stockId) {
+  try {
+    return request.delete(`${URL}/api/v1/stocks/${stockId}`);
+  } catch(e) {
+    return { error: e.message }
+  }
+}
