eff16e6af11bd05c2777249a8980f0d6893d2efc
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-10-08 11:33:14 -0700

    adds about us page

---
+++ src/AboutUs/Aboutus.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class About extends React.Component {
+  state = {  }
+  render() { 
+    return (
+      <div>
+        <h1>About the Developers</h1>
+        <div>Annalise</div>
+        
+      </div>
+    );
+  }
+}
+ 
+export default About;
